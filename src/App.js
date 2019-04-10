33da8a1f3dafcb43fdfa8166bc53e429d2d1c2d5
Shenae Simmons <shhnae@gmail.com>
2019-04-10 16:58:27 -0400

    i tried

--- src/App.js
+++ src/App.js
@@ -4,28 +4,47 @@ import './App.css';
 /* 
  * Perhaps some hints?
  *
- * 1. Use a constructor and initialize state inside of it
- * 2. Set up a fetch function in fetchAllGifs(). **Use your giphy api endpoint as url**
+ * 1. Use a constructor and initialize state inside of it*/
+ /*2. Set up a fetch function in fetchAllGifs(). **Use your giphy api endpoint as url**
  * 3. Call your fetch function inside of componentDidMount()
  * 4. Set up a loading message until promise is resolved, and fetch status: 200
  * 5. Maybe map through the fetched data and get them all to render on the page before you add the button functionality
  */
 
 class App extends Component {
-    // this.state = {
-    //   allGifs: [],
-    //   fetchOK: null
-    // }
+    constructor () {
+      super()
+      this.state = {
+        allGifs: [],
+        fetchOK: null
+      }
+  
+    }
 
   componentDidMount() {
-    // call fetch function here
+    this.fetchAllGifs();
   }
 
   fetchAllGifs() {
-    // declare a url variable for the Giphy API endpoint
-    // RETURN all fetched gifs!
+    return fetch('https://api.giphy.com/v1/gifs/random?api_key=7k1bt0XPnrLgImgvHYbPr4aoXb6Y6yhL&tag=&rating=PG')
+  
+    .then((resp) => {
+      if (!resp.ok) {
+        throw Error(resp.statusText);
+      }
+      return resp.json();
+    })
+    .then((data) => {
+      // console.log(JSON.stringify(data));
+      this.setState({ 
+        allGifs: data.data.image_url,
+        fetchOK: true
+      })
     // set state with the data you've fetched from the API to allGifs
+    return data.data.image_url
+    })
     // and set fetchOK to true.
+    // fetchOK = true 
   }
 
 
@@ -33,18 +52,22 @@ class App extends Component {
    * This function below could help you out :)
    */
 
-  // renderFetch() {
-  //   if (this.state.fetchOK) { return <h1>Done</h1> }
-  //   else { return <h1>Loading...</h1> }
-  // }
+  renderFetch() {
+    if (this.state.fetchOK) { return <h1>Done</h1> }
+    else { return <h1>Loading...</h1> }
+  }
 
   render() {
+    console.log(this.state.allGifs.data)
     return (
       <div className="App">
+      {this.renderFetch()}
         <header className="App-header">
           <h1 className="App-title">Welcome to React Giphy</h1>
         </header>
         <main>
+          <img src={this.state.allGifs} />
+          <button onClick= {this.fetchAllGifs}>Change GIF</button>
           I'm going to show a collection of gifs!
         </main>
       </div>
